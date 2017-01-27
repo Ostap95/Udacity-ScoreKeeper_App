f98aca5e17ddab7e43ae63f6071a6716dd4e5e73
Ostap95 <ostap_95@hotmail.com>
2017-01-27 14:05:31 +0000

    V1
    Initial Code

---
+++ app/src/main/java/com/ostap_kozak/scorekeeper/MainActivity.java
@@ -0,0 +1,30 @@
+package com.ostap_kozak.scorekeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int teamAScore = 0;
+    private int teamBScore = 0;
+
+    private TextView teamAName;
+    private TextView teamBName;
+    private TextView teamAScoreTextView;
+    private TextView teamBScoreTextView;
+
+    private
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
